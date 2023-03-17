0e49cd0d97f376614602fac9dea57b8297809145
RicardoSnz <73980150+ricardosnz@users.noreply.github.com>
2023-03-16 18:51:01 -0600

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
 import pomodoro from './pomodoro.jpg';
 import rickandmorty from './rickandmorty.jpg';
 import todo from './todo.jpg';
-import wheather from './wheather.jpg';
+import weather from './wheather.jpg';
 import profile from './profile.jpeg';
 import ricardo_cv from './ricardo_cv.pdf';
 
-export { pomodoro, rickandmorty, todo, wheather, profile, ricardo_cv };
+export { pomodoro, rickandmorty, todo, weather, profile, ricardo_cv };
